733ece7c2e56409449a5fe3719fa85913bccf986
GThapa2018 <gthapa8@gmail.com>
2024-09-24 23:51:29 -0400

    uploading init changes for project structure.

---
+++ src/main/java/com/example/DevExperimentation/DevExperimentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevExperimentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevExperimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevExperimentationApplication.class, args);
+	}
+
+}
